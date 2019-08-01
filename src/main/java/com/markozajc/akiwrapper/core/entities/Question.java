875ffe07776d6bf5156905902795df1398bda8df
Marko Zajc <marko.zajc@protonmail.com>
2019-08-01 02:36:22 +0200

    Annotate Question

--- src/main/java/com/markozajc/akiwrapper/core/entities/Question.java
+++ src/main/java/com/markozajc/akiwrapper/core/entities/Question.java
@@ -1,5 +1,8 @@
 package com.markozajc.akiwrapper.core.entities;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
 import com.markozajc.akiwrapper.Akiwrapper.Answer;
 
 /**
@@ -14,22 +17,26 @@ public interface Question extends Identifiable {
 	 * @return current completion percentage (as a double). Higher means closer to the
 	 *         answer
 	 */
+	@Nonnegative
 	double getProgression();
 
 	/**
 	 * @return current step (question number). This uses zero-based index, meaning the
 	 *         first question will be on step {@code 0}
 	 */
+	@Nonnegative
 	int getStep();
 
 	/**
 	 * @return gain from the last question
 	 */
+	@Nonnegative
 	double getGain();
 
 	/**
 	 * @return the actual question the user should answer to
 	 */
+	@Nonnull
 	String getQuestion();
 
 }
