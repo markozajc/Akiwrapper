58c87167fcb82ca85f06a00397ec4ba011570ae7
Marko Zajc <marko.zajc@protonmail.com>
2020-07-06 19:52:52 +0200

    Update Server.java
    Add GuessType (subject), update documentation.

--- src/main/java/com/markozajc/akiwrapper/core/entities/Server.java
+++ src/main/java/com/markozajc/akiwrapper/core/entities/Server.java
@@ -18,27 +18,63 @@ public interface Server {
 	 */
 	@SuppressWarnings("javadoc")
 	public enum Language {
-		ARABIC,
-		CHINESE,
-		DUTCH,
-		ENGLISH,
-		FRENCH,
-		GERMAN,
-		HEBREW,
-		ITALIAN,
-		JAPANESE,
-		KOREAN,
-		MALAY,
-		POLISH,
-		PORTUGUESE,
-		RUSSIAN,
-		SPANISH,
-		TURKISH,
+
+		ARABIC("ar"),
+		CHINESE("cn"),
+		DUTCH("nl"),
+		ENGLISH("en"),
+		FRENCH("fr"),
+		GERMAN("de"),
+		HEBREW("il"),
+		INDONESIAN("id"),
+		ITALIAN("it"),
+		JAPANESE("jp"),
+		KOREAN("kr"),
+		POLISH("pl"),
+		PORTUGUESE("pt"),
+		RUSSIAN("ru"),
+		SPANISH("es"),
+		TURKISH("tr");
+
+		private final String id;
+
+		Language(String id) {
+			this.id = id;
+		}
+
+		public String getId() {
+			return this.id;
+		}
 	}
 
 	/**
-	 * @return the base (API's) URL for this server
-	 * 
+	 * Server's guess type (referred to as the "subject" in the API). Decides what kind
+	 * of things server's guesses will represent.
+	 *
+	 * @author Marko Zajc
+	 */
+	@SuppressWarnings("javadoc")
+	public enum GuessType {
+
+		ANIMAL("animals new"),
+		CHARACTER("characters"),
+		OBJECT("objects");
+
+		private final String label;
+
+		GuessType(String label) {
+			this.label = label;
+		}
+
+		public String getLabel() {
+			return this.label;
+		}
+
+	}
+
+	/**
+	 * @return The base (API's) URL for this server.
+	 *
 	 * @deprecated Changed for clarification. Use {@link #getApiUrl()} instead.
 	 */
 	@Deprecated
@@ -48,7 +84,7 @@ public interface Server {
 	}
 
 	/**
-	 * @return the base (API's) URL for this server
+	 * @return The base (API's) URL for this server.
 	 */
 	@SuppressWarnings("null")
 	@Nonnull
@@ -57,24 +93,31 @@ public interface Server {
 	}
 
 	/**
-	 * @return the bare host for this server (in a {@code hostname:port} format)
+	 * @return The bare host for this server (in a {@code hostname:port} format).
 	 */
 	@Nonnull
 	String getHost();
 
 	/**
-	 * @return this server's localization language. The server will return localized
-	 *         elements (eg. questions) depending on its localization language
+	 * @return This server's localization language. The server will return localized
+	 *         elements (eg. questions) depending on its localization language.
 	 */
 	@Nonnull
 	Language getLocalization();
 
 	/**
+	 * @return This server's guess type. The server's guesses will be based on the
+	 *         subject type.
+	 */
+	@Nonnull
+	GuessType getSubject();
+
+	/**
 	 * Check if the current {@link Server} is still available. This is a shortcut for
-	 * {@link Servers#isUp(Server)}
+	 * {@link Servers#isUp(Server)}.
 	 *
-	 * @return true if that API server is available, false if not
-	 * 
+	 * @return True if that API server is available, false if not.
+	 *
 	 * @see Servers#isUp(Server)
 	 */
 	default boolean isUp() {
