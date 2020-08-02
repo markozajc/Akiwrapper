3104602bbbc52e0eafa9cd6656c609a28653ea19
Marko Zajc <marko.zajc@protonmail.com>
2020-08-02 18:42:07 +0200

    Apply some more tweaks
    Remove the Log4J to SLF4J bridge, tweak documentation, fix a bug.

--- src/main/java/com/markozajc/akiwrapper/AkiwrapperBuilder.java
+++ src/main/java/com/markozajc/akiwrapper/AkiwrapperBuilder.java
@@ -2,6 +2,9 @@ package com.markozajc.akiwrapper;
 
 import javax.annotation.Nonnull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.markozajc.akiwrapper.core.entities.AkiwrapperMetadata;
 import com.markozajc.akiwrapper.core.entities.Server;
 import com.markozajc.akiwrapper.core.entities.Server.GuessType;
@@ -20,6 +23,8 @@ import com.markozajc.akiwrapper.core.utils.Servers;
  * @author Marko Zajc
  */
 public class AkiwrapperBuilder extends MutableAkiwrapperMetadata {
+
+	private static final Logger LOG = LoggerFactory.getLogger(AkiwrapperBuilder.class);
 
 	/**
 	 * Creates a new AkiwrapperBuilder object. The default server used is the first
@@ -70,15 +75,22 @@ public class AkiwrapperBuilder extends MutableAkiwrapperMetadata {
 	public Akiwrapper build() throws ServerNotFoundException {
 		Server server = findServer();
 		if (server instanceof ServerList) {
+			ServerList serverList = (ServerList) server;
+			int count = serverList.getRemainingSize() + 1;
 			do {
+				LOG.debug("Using server {} out of {} from the list.", count - serverList.getRemainingSize(), count);
 				try {
 					return new AkiwrapperImpl(server, this.filterProfanity);
+				} catch (ServerUnavailableException e) { // NOSONAR v
+					LOG.debug("Server seems to be down.");
+					// We can safely ignore this, let's just iterate to the next instance.
 				} catch (RuntimeException e) {
-
+					LOG.warn("Failed to construct an instance, trying the next available server", e);
 				}
-			} while (((ServerList) server).next());
+			} while (serverList.next());
 			throw new ServerUnavailableException("AW-KO MULTIPLE FAILS");
 		} else {
+			LOG.debug("Given Server is not a ServerList, only attempting to build once.");
 			return new AkiwrapperImpl(server, this.filterProfanity);
 		}
 	}
