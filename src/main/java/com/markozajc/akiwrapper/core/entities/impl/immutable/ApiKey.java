834a2f2a23ebef27405ad6ba2db9769e84d4d2f1
Marko Zajc <marko@zajc.eu.org>
2022-07-04 21:42:49 +0200

    Update ApiKey.java
    Include the full page in the exception and use a locally provided
    UnirestInstance

--- src/main/java/com/markozajc/akiwrapper/core/entities/impl/immutable/ApiKey.java
+++ src/main/java/com/markozajc/akiwrapper/core/entities/impl/immutable/ApiKey.java
@@ -1,17 +1,26 @@
 package com.markozajc.akiwrapper.core.entities.impl.immutable;
 
-import java.io.UnsupportedEncodingException;
+import static com.markozajc.akiwrapper.core.Route.BASE_AKINATOR_URL;
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.regex.Pattern.compile;
+
 import java.net.URLEncoder;
-import java.util.regex.*;
+import java.util.Base64;
+import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
 
-import com.markozajc.akiwrapper.core.Route;
+import kong.unirest.UnirestInstance;
 
 public class ApiKey {
 
+	private static final String EXCEPTION_NO_KEY = "Couldn't find the API key!" +
+		"Please consider opening a new ticket at https://github.com/markozajc/Akiwrapper/issues." +
+		"Base64 encoded page: %s";
+
 	private static final Pattern API_KEY_PATTERN =
-		Pattern.compile("var uid_ext_session = '(.*)'\\;\\n.*var frontaddr = '(.*)'\\;");
+		compile("var uid_ext_session = '(.*)'\\;\\n.*var frontaddr = '(.*)'\\;");
 
 	private static final String FORMAT = "frontaddr=%s&uid_ext_session=%s";
 
@@ -25,25 +34,23 @@ public class ApiKey {
 		this.frontAddress = frontAddress;
 	}
 
-	@SuppressWarnings("null")
 	@Nonnull
-	public String compile() {
-		try {
-			return String.format(FORMAT, URLEncoder.encode(this.frontAddress, "UTF-8"), this.sessionUid);
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e); // never throws, UTF-8 is always supported
-		}
+	@SuppressWarnings("null")
+	public String querystring() {
+		return format(FORMAT, URLEncoder.encode(this.frontAddress, UTF_8), this.sessionUid);
 	}
 
 	@SuppressWarnings("null")
-	public static ApiKey accquireApiKey() {
-		Matcher matcher =
-			API_KEY_PATTERN.matcher(Route.UNIREST.get(Route.BASE_AKINATOR_URL + "/game").asString().getBody());
-		if (!matcher.find())
-			throw new IllegalStateException("Couldn't find the API key! Please consider opening" +
-				" a new ticket at https://github.com/markozajc/Akiwrapper/issues.");
+	public static ApiKey accquireApiKey(UnirestInstance unirest) {
+		var page = unirest.get(BASE_AKINATOR_URL + "/game").asString().getBody();
+		var matcher = API_KEY_PATTERN.matcher(page);
+		if (matcher.find()) {
+			return new ApiKey(matcher.group(1), matcher.group(2));
 
-		return new ApiKey(matcher.group(1), matcher.group(2));
+		} else {
+			throw new IllegalStateException(format(EXCEPTION_NO_KEY,
+												   Base64.getEncoder().encodeToString(page.getBytes(UTF_8))));
+		}
 	}
 
 }
