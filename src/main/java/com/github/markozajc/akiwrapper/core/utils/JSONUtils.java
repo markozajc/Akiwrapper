f72f93d7b729ad6cea87cbc78948e9d4ae930f2f
Marko Zajc <marko@zajc.eu.org>
2022-07-07 18:38:24 +0200

    Change namespace to com.github.markozajc.akiwrapper

---
+++ src/main/java/com/github/markozajc/akiwrapper/core/utils/JSONUtils.java
@@ -0,0 +1,93 @@
+package com.github.markozajc.akiwrapper.core.utils;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
+
+import java.util.*;
+
+import javax.annotation.Nonnull;
+
+import org.json.JSONObject;
+
+/**
+ * A set of utilities for JSON that prevent type errors.
+ *
+ * @author Marko Zajc
+ */
+public final class JSONUtils {
+
+	private JSONUtils() {}
+
+	/**
+	 * Gets an {@link Integer} value from a {@link JSONObject}.
+	 *
+	 * @param json
+	 * @param key
+	 *
+	 * @return {@link Optional} integer value
+	 */
+	@Nonnull
+	@SuppressWarnings("null")
+	public static OptionalInt getInteger(@Nonnull JSONObject json, @Nonnull String key) {
+		if (!json.has(key))
+			return OptionalInt.empty();
+
+		var value = json.get(key);
+		if (value instanceof Number) {
+			return OptionalInt.of(((Number) value).intValue());
+
+		} else if (value instanceof String) {
+			return OptionalInt.of(parseInt((String) value));
+
+		} else {
+			throw new NumberFormatException(format("Could not format \"%s\" of type %s into an int", value,
+												   value.getClass().getName()));
+		}
+	}
+
+	/**
+	 * Gets a {@link Double} value from a {@link JSONObject}.
+	 *
+	 * @param json
+	 * @param key
+	 *
+	 * @return {@link Optional} double value
+	 */
+	@Nonnull
+	@SuppressWarnings("null")
+	public static OptionalDouble getDouble(@Nonnull JSONObject json, @Nonnull String key) {
+		if (!json.has(key))
+			return OptionalDouble.empty();
+
+		var value = json.get(key);
+		if (value instanceof Number) {
+			return OptionalDouble.of(((Number) value).doubleValue());
+
+		} else if (value instanceof String) {
+			return OptionalDouble.of(parseDouble((String) value));
+
+		} else {
+			throw new NumberFormatException(format("Could not format \"%s\" of type %s into a double", value,
+												   value.getClass().getName()));
+		}
+	}
+
+	/**
+	 * Gets a {@link String} value from a {@link JSONObject}.
+	 *
+	 * @param json
+	 * @param key
+	 *
+	 * @return {@link Optional} string value
+	 */
+	@Nonnull
+	@SuppressWarnings("null")
+	public static Optional<String> getString(@Nonnull JSONObject json, @Nonnull String key) {
+		if (!json.has(key))
+			return Optional.empty();
+		else
+			return Optional.of(json.get(key).toString());
+	}
+
+}
---
+++ src/test/java/com/github/markozajc/akiwrapper/IntegrationTest.java
@@ -0,0 +1,117 @@
+package com.github.markozajc.akiwrapper;
+
+import static java.lang.String.format;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.annotation.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+import org.slf4j.Logger;
+
+import com.github.markozajc.akiwrapper.Akiwrapper.Answer;
+import com.github.markozajc.akiwrapper.core.entities.*;
+import com.github.markozajc.akiwrapper.core.entities.Server.*;
+import com.github.markozajc.akiwrapper.core.exceptions.ServerNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegrationTest {
+
+	private static final String SERVER_GUESSTYPE_NO_MATCH =
+		"The wanted and actual guess type of the server don't match.";
+	private static final String SERVER_LANGUAGE_NO_MATCH = "The wanted and actual language of the server don't match.";
+	private static final String QUESTION_CURRENT_NO_MATCH =
+		"Current question does not match the one just returned by the API.";
+	private static final String QUESTION_WRONG_STEP = "Question was on an unexpected step.";
+	private static final String QUESTION_EMPTY = "Question mustn't be empty.";
+	private static final String QUESTION_NULL = "Question was null";
+	private static final String QUESTION_INITIAL_NO_MATCH =
+		"Initial question does not match the one after an equal amount of answers and undoes.";
+
+	@ParameterizedTest
+	@MethodSource("generateTestAkiwrapper")
+	void testAkiwrapper(@Nonnull Language language, @Nonnull GuessType guessType) {
+		Logger log = getLogger(format("%s-%s", language, guessType));
+		log.info("Establishing connection");
+		Akiwrapper api;
+		try {
+			api = new AkiwrapperBuilder().setLanguage(language).setGuessType(guessType).build();
+		} catch (ServerNotFoundException e) {
+			log.warn("Current combination not supported, server wasn't found.");
+			log.trace("", e);
+			return;
+		}
+
+		log.info("Asserting the current state.");
+		Question initialQuestion = api.getQuestion();
+		int expectedState = 0;
+		checkQuestion(initialQuestion, expectedState);
+		assertEquals(language, api.getServer().getLanguage(), SERVER_LANGUAGE_NO_MATCH);
+		assertEquals(guessType, api.getServer().getGuessType(), SERVER_GUESSTYPE_NO_MATCH);
+		log.trace("API server URL: {}", api.getServer().getUrl());
+
+		log.info("Advancing {} steps (one time for each possible answer).", Answer.values().length);
+		for (Answer answer : Answer.values()) {
+			log.debug("Answering with {} and checking the question.", answer.name());
+			Question newQuestion = api.answer(answer);
+			assertEquals(newQuestion, api.getQuestion(), QUESTION_CURRENT_NO_MATCH);
+			expectedState++;
+			checkQuestion(api.getQuestion(), expectedState);
+		}
+
+		log.info("Fetching guesses.", Answer.values().length);
+		List<Guess> guesses = api.getGuesses();
+		debugGuesses(log, guesses);
+
+		log.info("Advancing -{} steps (using undo).", Answer.values().length);
+		for (int i = 0; i < Answer.values().length; i++) {
+			Question undoneQuestion = api.undoAnswer();
+			assertEquals(undoneQuestion, api.getQuestion(), QUESTION_CURRENT_NO_MATCH);
+			expectedState--;
+			checkQuestion(api.getQuestion(), expectedState);
+		}
+
+		log.info("Asserting the final state.");
+		assertNull(api.undoAnswer());
+		checkQuestion(api.getQuestion(), 0);
+		Question currentQuestion = api.getQuestion();
+		if (initialQuestion != null && currentQuestion != null) {
+			assertEquals(initialQuestion.getQuestion(), currentQuestion.getQuestion(), QUESTION_INITIAL_NO_MATCH);
+		}
+		// Neither of those can be null due to checkQuestion checking (and failing on)
+		// nullability.
+
+	}
+
+	private static void debugGuesses(Logger log, List<Guess> guesses) {
+		log.debug("There are {} guesses.", guesses.size());
+		for (Guess guess : guesses) {
+			log.trace("{} - {}", guess.getProbability(), guess.getName());
+		}
+	}
+
+	private static void checkQuestion(@Nullable Question question, int expectedState) {
+		if (question == null) {
+			fail(QUESTION_NULL);
+		} else {
+			assertFalse(question.getQuestion().isEmpty(), QUESTION_EMPTY);
+			assertEquals(expectedState, question.getStep(), QUESTION_WRONG_STEP);
+		}
+	}
+
+	private static Stream<Arguments> generateTestAkiwrapper() {
+		Arguments[] arguments = new Arguments[Language.values().length * GuessType.values().length];
+		int i = 0;
+		for (Language lang : Language.values())
+			for (GuessType guessType : GuessType.values()) {
+				arguments[i] = Arguments.of(lang, guessType);
+				i++;
+			}
+		return Stream.of(arguments);
+	}
+
+}
