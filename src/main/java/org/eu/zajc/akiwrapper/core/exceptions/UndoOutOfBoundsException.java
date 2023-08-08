97e7433b9ead04752ec0e82131ecde915f67605e
Marko Zajc <marko@zajc.eu.org>
2023-08-09 00:01:56 +0200

    Update the licensing information

--- src/main/java/org/eu/zajc/akiwrapper/core/exceptions/UndoOutOfBoundsException.java
+++ src/main/java/org/eu/zajc/akiwrapper/core/exceptions/UndoOutOfBoundsException.java
@@ -3,18 +3,16 @@
  * Akiwrapper, the Java API wrapper for Akinator
  * Copyright (C) 2017-2023 Marko Zajc
  *
- * This program is free software; you can redistribute it and/or modify it under the
+ * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 3 of the License, or (at your option) any later
- * version.
+ * Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with this
- * program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street,
- * Fifth Floor, Boston, MA 02110-1301, USA.
+ * program. If not, see <https://www.gnu.org/licenses/>.
  */
 package org.eu.zajc.akiwrapper.core.exceptions;
 
